3a73fad77e76bfe535289071dda5922a32099bdc
Wesley Silva <wesleylaudelino@outlook.com>
2017-09-25 19:41:48 -0400

    Criacao Classe/Controller

---
+++ VeiculoManutencao/Models/Automovel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VeiculoManutencao.Models
+{
+    public class Automovel
+    {
+        public int AutomovelId { get; set; }
+        public string Tipo { get; set; }
+        public string Descricao { get; set; }
+        public string Marca { get; set; }
+        public Boolean Disponivel { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
